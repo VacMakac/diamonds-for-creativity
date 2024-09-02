9b7a0761a59afc58be6894fc95898eb01d4dc803
VacMakac <102311913+VacMakac@users.noreply.github.com>
2024-09-02 16:11:53 +0400

    Add files via upload

--- core/scripts.js
+++ core/scripts.js
@@ -1,6 +1,6 @@
 const apiKey = 'AIzaSyCzG6i6Vyo1ZFxqwVy5F224mbxf5BnF-v0';
 const sheetId = '1f0w2MOL4Z_vlrFUxj9wgTBd5_4AG8Q9HSPrOhPePrZ8';
-const range = 'CORE!A2:J';
+const range = 'CORE123!A2:L';
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
@@ -29,8 +29,10 @@ function fetchData() {
                     const overallRating = parseFloat(row[5]) || 0;
                     const vacRating = parseFloat(row[6]) || 0;
                     const vipsRating = parseFloat(row[7]) || 0;
-                    const vacVideo = row[8] || '';
-                    const vipsVideo = row[9] || '';
+                    const smashRating = parseFloat(row[8]) || 0;
+                    const vacVideo = row[9] || '';
+                    const vipsVideo = row[10] || '';
+                    const smashVideo = row[11] || '';
 
                     const tileElement = document.createElement('div');
                     tileElement.classList.add('table-tile');
@@ -43,7 +45,7 @@ function fetchData() {
                 `;                
                     
                     tileElement.addEventListener('click', () => {
-                        showMenu(title, link, overallRating, vacRating, vipsRating, vacVideo, vipsVideo);
+                        showMenu(title, link, overallRating, vacRating, vipsRating, smashRating, vacVideo, vipsVideo, smashVideo);
                     });
                     
                     container.appendChild(tileElement);
@@ -55,7 +57,7 @@ function fetchData() {
         .catch(error => console.error('Ошибка при загрузке данных:', error));
 }
 
-function showMenu(title, link, overallRating, vacRating, vipsRating, vacVideo, vipsVideo) {
+function showMenu(title, link, overallRating, vacRating, vipsRating, smashRating, vacVideo, vipsVideo, smashVideo) {
     document.getElementById('modal-title').textContent = title;
     const videoLink = document.getElementById('modal-link');
     videoLink.href = link;
@@ -63,15 +65,43 @@ function showMenu(title, link, overallRating, vacRating, vipsRating, vacVideo, v
 
     const vacVideoElement = document.getElementById('vac-video');
     const vipsVideoElement = document.getElementById('vips-video');
+    const smashVideoElement = document.getElementById('smash-video');
 
-    vacVideoElement.src = `https://i.ytimg.com/vi/${vacVideo}/maxresdefault.jpg`;
-    vacVideoElement.onclick = () => window.open(`https://www.youtube.com/watch?v=${vacVideo}`, '_blank');
+    // Оценка ВакМакака
+    if (vacRating) {
+        document.getElementById('vac-rating-text').innerHTML = `${vacRating}<span class="rating-max">/90</span>`;
+        vacVideoElement.src = `https://i.ytimg.com/vi/${vacVideo}/maxresdefault.jpg`;
+        vacVideoElement.style.display = 'block';
+        vacVideoElement.onclick = () => window.open(`https://www.youtube.com/watch?v=${vacVideo}`, '_blank');
+        document.querySelector('.vac-section').style.display = 'flex';
+    } else {
+        vacVideoElement.style.display = 'none';
+        document.querySelector('.vac-section').style.display = 'none';
+    }
 
-    vipsVideoElement.src = `https://i.ytimg.com/vi/${vipsVideo}/maxresdefault.jpg`;
-    vipsVideoElement.onclick = () => window.open(`https://www.youtube.com/watch?v=${vipsVideo}`, '_blank');
+    // Оценка Випса
+    if (vipsRating) {
+        document.getElementById('vips-rating-text').innerHTML = `${vipsRating}<span class="rating-max">/90</span>`;
+        vipsVideoElement.src = `https://i.ytimg.com/vi/${vipsVideo}/maxresdefault.jpg`;
+        vipsVideoElement.style.display = 'block';
+        vipsVideoElement.onclick = () => window.open(`https://www.youtube.com/watch?v=${vipsVideo}`, '_blank');
+        document.querySelector('.vips-section').style.display = 'flex';
+    } else {
+        vipsVideoElement.style.display = 'none';
+        document.querySelector('.vips-section').style.display = 'none';
+    }
 
-    document.getElementById('vac-rating-text').innerHTML = `${vacRating}<span class="rating-max">/90</span>`;
-    document.getElementById('vips-rating-text').innerHTML = `${vipsRating}<span class="rating-max">/90</span>`;
+    // Оценка Смэша
+    if (smashRating) {
+        document.getElementById('smash-rating-text').innerHTML = `${smashRating}<span class="rating-max">/90</span>`;
+        smashVideoElement.src = `https://i.ytimg.com/vi/${smashVideo}/maxresdefault.jpg`;
+        smashVideoElement.style.display = 'block';
+        smashVideoElement.onclick = () => window.open(`https://www.youtube.com/watch?v=${smashVideo}`, '_blank');
+        document.querySelector('.smash-section').style.display = 'flex';
+    } else {
+        smashVideoElement.style.display = 'none';
+        document.querySelector('.smash-section').style.display = 'none';
+    }
 
     const modal = document.getElementById('modal');
     modal.style.display = 'flex';
