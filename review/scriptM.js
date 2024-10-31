7c8d4551f4045dc5e0e1ef7f0b988edd2e56879d
VacMakac <102311913+VacMakac@users.noreply.github.com>
2024-10-31 21:21:58 +0400

    Add files via upload

---
+++ review/scriptM.js
@@ -0,0 +1,61 @@
+// Функция для обновления оценки слайдерами
+const sliders = document.querySelectorAll('.slider');
+const totalScoreElement = document.getElementById('totalScore');
+
+sliders.forEach(slider => {
+    slider.addEventListener('input', updateScore);
+});
+
+function updateScore() {
+    let score = 0;
+
+    score += parseInt(document.getElementById('slider1').value);
+    score += parseInt(document.getElementById('slider2').value);
+    score += parseInt(document.getElementById('slider3').value);
+    score += parseInt(document.getElementById('slider4').value);
+
+    let multiplier1 = parseInt(document.getElementById('slider5').value) * 0.15;
+    let multiplier2 = parseInt(document.getElementById('slider6').value) * 0.10;
+
+    score *= (1 + multiplier1 + multiplier2);
+    score = Math.min(Math.round(score), 90);
+
+    totalScoreElement.textContent = score;
+
+    document.getElementById('slider1Value').textContent = document.getElementById('slider1').value;
+    document.getElementById('slider2Value').textContent = document.getElementById('slider2').value;
+    document.getElementById('slider3Value').textContent = document.getElementById('slider3').value;
+    document.getElementById('slider4Value').textContent = document.getElementById('slider4').value;
+    document.getElementById('slider5Value').textContent = document.getElementById('slider5').value;
+    document.getElementById('slider6Value').textContent = document.getElementById('slider6').value;
+}
+
+// Сброс значений слайдеров и общей оценки
+function resetSliders() {
+    sliders.forEach(slider => {
+        slider.dispatchEvent(new Event('input')); // Обновляем отображение значений
+    });
+    totalScoreElement.textContent = 25; // Устанавливаем значение по умолчанию
+}
+
+// Сохранение оценки
+document.getElementById('saveButton').addEventListener('click', function () {
+    saveScore().then(() => {
+        window.location.reload();
+    });
+});
+
+function saveScore() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Оценка успешно сохранена');
+            resolve();
+        }, 0);
+    });
+}
+
+// Инициализация
+document.addEventListener('DOMContentLoaded', function () {
+    loadVideosFromGoogleSheets();
+    resetSliders(); // Устанавливаем значения по умолчанию при загрузке
+});
